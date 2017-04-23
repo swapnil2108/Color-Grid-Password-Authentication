b3aa7ebf4a1c68ba1ba51ffb644e60c25f25c027
Swapnil2108 <swapnil2108@csu.fullerton.edu>
2017-04-23 06:27:56 -0700

    server updated

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
 var url = 'mongodb://localhost:27017/test';
 
+
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
   //__dirname : It will resolve to your project folder.
@@ -28,6 +30,57 @@ app.get('/signup', function(req, res) {
 });
 app.get('/login', function(req, res) {
   res.sendFile(path.join(__dirname + '/html/login.html'));
+});
+app.get('/index', function(req, res) {
+  res.sendFile(path.join(__dirname + '/html/index.html'));
+});
+app.post('/login',function(req,res){
+var data = JSON.parse(Object.keys(req.body)[0]);
+// console.log(data.user);
+MongoClient.connect(url,function(err,db){
+  if(err){
+    console.log("unable to connect",err);
+  }else {
+    console.log("connection SUCCESS",url);
+    var collection = db.collection('swapnil');
+    collection.findOne({"user" : data.user},function(err,document){
+      if(err){
+        console.log(err);
+        res.send("null")
+      }else{
+        console.log(document);
+        res.send(document);
+      }
+    });
+  }
+  db.close();
+});
+});
+app.post('/signup', function(req, res) {
+  var data = JSON.parse(Object.keys(req.body)[0]);
+  console.log(data);
+  MongoClient.connect(url, function(err, db) {
+    if (err) {
+      console.log("Unabel to connect to mongo server ERROR : ", err);
+    } else {
+      console.log("Connection sucesful to ", url);
+      var collection = db.collection('swapnil');
+          collection.insert(
+            data
+          , function(err, result) {
+            if (err) {
+              console.log("ERROR ", err);
+              res.send(err);
+              db.close();
+            } else {
+              console.log("SUCCESS INSERTED in to users collection _is are ", result.length, result);
+              res.send(result);
+              db.close();
+            }
+      });
+    }
+    db.close();
+  });
 });
 app.listen(3000);
 console.log("Running at Port 3000");
